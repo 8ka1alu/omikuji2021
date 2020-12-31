ef410e4d78d27d094e94833665a46a8d40c9775a
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-12-31 15:40:05 +0900

    Update and rename test.py to omikuji.py

--- cogs/omikuji.py
+++ cogs/omikuji.py
@@ -1,14 +1,16 @@
 import discord
 from discord.ext import commands
+import r
+import json
+import random
 
 class test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    @commands.has_permissions(administrator=True)
+    @commands.command(name="おみくじ")
     @commands.cooldown(1, 30, commands.BucketType.user)
-    async def test(self, ctx):
+    async def omi1(self, ctx):
         print("test")
 
 def setup(bot):
